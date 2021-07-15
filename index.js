dc82024e243ef4f3c374774e1c3b9ed0420c073a
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-07-15 18:53:21 -0400

    commit tree 84b27186eb508698ecc0f96afa70cf0bee2ce50d

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Your code here
+class Polygon {
+
+    constructor(arrayOfSides) {
+        this.sides = arrayOfSides;
+    }
+
+    get countSides () {
+        return this.sides.length
+    }
+
+    get perimeter() {
+        return this.sides.reduce((accumulator, currentValue) => {
+           return currentValue + accumulator
+        }, 0);
+    }
+}
+
+class Triangle extends Polygon {
+    
+    get isValid () {
+        if (this.sides[0] + this.sides[1] > this.sides[2] 
+            && this.sides[1] + this.sides[2] > this.sides[0]
+            && this.sides[0] + this.sides[2] > this.sides[1]) {
+                return true
+            }
+        else {
+            return false
+        }
+    }
+}
+
+class Square extends Polygon {
+
+    get isValid () {
+        if (this.sides[0] === this.sides[1]
+            && this.sides[1] === this.sides[2]
+            && this.sides[2] === this.sides [3])
+        { return true }
+        else
+        { return false }
+    }
+    
+    get area () {
+        return  this.sides[0] * this.sides[1];
+    }
+}
